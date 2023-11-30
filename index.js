05f9b404e49e82bebdb38f63c6c4193460b398e2
kyw0300 <kyw0300@naver.com>
2023-11-30 23:57:16 +0900

    Feat:231130 Deep Dive Startgit add .!

---
+++ index.js
@@ -0,0 +1,8 @@
+const arr = [1, 2, 3];
+
+// arr.forEach(console.log);
+
+// terminal을 열고 node index 명령어로 javascript 파일을 실행할 수 있다.
+// code runner 플러그인으로도 javascript 파일 실행 가능 ctrl + alt + n
+
+arr.forEach(alert);
