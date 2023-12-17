af5739c5c073f9cafb3ad98f216f79323dc3f8a4
kyw0300 <kyw0300@naver.com>
2023-12-17 14:11:58 +0900

    Feat:231217 15 letConst.js 추가 var vs let vs const

---
+++ 15_letConst/letConst.js
@@ -0,0 +1,158 @@
+var x = 1;
+var y = 1;
+
+// var 키워드로 선언된 변수는 같은 스코프 내에서 중복 선언을 허용한다.
+// 초기화문이 있는 변수 선언문은 자바스크립트 엔진에 의해 var 키워드가 없는 것처럼 동작한다.
+var x = 100;
+// 초기화문이 없는 변수 선언문은 무시된다.
+var y;
+
+console.log(x); // 100
+console.log(y); // 1
+
+
+var x = 1;
+
+if (true) {
+    // x는 전역 변수다. 이미 선언된 전역 변수 x가 있으므로 x 변수는 중복 선언된다.
+    // 이는 의도치 않게 변수값이 변경되는 부작용을 발생시킨다.
+    var x = 10;
+}
+
+console.log(x); // 10
+
+var i = 10;
+
+// for문에서 선언한 i는 전역 변수이다. 이미 선언된 전역 변수 i가 있으므로 중복 선언된다.
+for (var i = 0; i < 5; i++) {
+    console.log(i); // 0 1 2 3 4
+}
+
+// 의도치 않게 i 변수의 값이 변경되었다.
+console.log(i); // 5
+
+// 함수 레벨 스코프는 전역 변수를 남발할 가능성을 높인다. 이로 인해 의도치 않게 전역 변수가 중복 선언되는 경우가 발생한다.
+
+
+// 이 시점에는 변수 호이스팅에 의해 이미 foo 변수가 선언되었다(1. 선언 단계)
+// 변수 foo는 undefined로 초기화된다. (2. 초기화 단계)
+console.log(foo); // undefined
+
+// 변수에 값을 할당(3. 할당 단계)
+foo = 123;
+
+console.log(foo); // 123
+
+// 변수 선언은 런타임 이전에 자바스크립트 엔진에 의해 암묵적으로 실행된다.
+var foo;
+
+
+var foo = 123;
+// var 키워드로 선언된 변수는 같은 스코프 내에서 중복 선언을 허용한다.
+// 아래 변수 선언문은 자바스크립트 엔진에 의해 var 키워드가 없는 것처럼 동작한다.
+var foo = 456;
+
+let bar = 123;
+// let이나 const 키워드로 선언된 변수는 같은 스코프 내에서 중복 선언을 허용하지 않는다.
+// let bar = 456; // SyntaxError: Identifier 'bar' has already been declared
+
+
+// var 키워드로 선언한 변수는 런타임 이전에 선언 단계와 초기화 단계가 실행된다.
+// 따라서 변수 선언문 이전에 변수를 참조할 수 있다.
+console.log(foo); // undefined
+
+var foo;
+console.log(foo); // undefined
+
+foo = 1; // 할당문에서 할당 단계가 실행된다.
+console.log(foo); // 1
+
+
+// 런타임 이전에 선언 단계가 실행된다. 아직 변수가 초기화되지 않았다.
+// 초기화 이전의 일시적 사각 지대에서는 변수를 참조할 수 없다.
+// console.log(foo); // ReferenceError: foo is not defined
+
+// let foo; // 변수 선언문에서 초기화 단계가 실행된다.
+// console.log(foo); // undefined
+
+// foo = 1; // 할당문에서 할당 단계가 실행된다.
+// console.log(foo); // 1
+
+
+// let foo = 1; // 전역 변수
+// {
+//   console.log(foo); // ReferenceError: Cannot access 'foo' before initialization
+//   let foo = 2; // 지역 변수
+// }
+
+
+// 자바스크립트는 ES6에서 도입된 let, const를 포함해서 모든 선언(var, let, const, function, function*, class 등)을 호이스팅한다.
+// 단, ES6에서 도입된 let, const, class를 사용한 선언문은 호이스팅이 발생하지 않는 것처럼 동작한다.
+
+
+// 전역 변수
+var x = 1;
+// 암묵적 전역
+y = 2;
+// 전역 함수
+function foo() { }
+
+// var 키워드로 선언한 전역 변수는 전역 객체 window의 프로퍼티다.
+console.log(window.x); // 1
+// 전역 객체 window의 프로퍼티는 전역 변수처럼 사용할 수 있다.
+console.log(x); // 1
+
+// 암묵적 전역은 전역 객체 window의 프로퍼티다.
+console.log(window.y); // 2
+console.log(y); // 2
+
+// 함수 선언문으로 정의한 전역 함수는 전역 객체 window의 프로퍼티다.
+console.log(window.foo); // ƒ foo() {}
+// 전역 객체 window의 프로퍼티는 전역 변수처럼 사용할 수 있다.
+console.log(foo); // ƒ foo() {}
+
+
+// const 키워드 : 반드시 선언과 동시에 초기화해야 한다.
+// 재할당 금지
+
+// 세전 가격
+// let preTaxPrice = 100;
+
+// // 세후 가격
+// // 0.1의 의미를 명확히 알기 어렵기 때문에 가독성이 좋지 않다.
+// let afterTaxPrice = preTaxPrice + (preTaxPrice * 0.1);
+
+// console.log(afterTaxPrice); // 110
+
+// 세율을 의미하는 0.1은 변경할 수 없는 상수로서 사용될 값이다.
+// 변수 이름을 대문자로 선언해 상수임을 명확히 나타낸다.
+const TAX_RATE = 0.1;
+
+// 세전 가격
+let preTaxPrice = 100;
+
+// 세후 가격
+let afterTaxPrice = preTaxPrice + (preTaxPrice * TAX_RATE);
+
+console.log(afterTaxPrice); // 110
+
+
+const person = {
+    name: 'Lee'
+};
+
+// 객체는 변경 가능한 값이다. 따라서 재할당없이 변경이 가능하다.
+person.name = 'Kim';
+
+console.log(person); // {name: "Kim"}
+
+
+// var vs. let vs. const
+// 변수 선언에는 기본적으로 const를 사용하고 let은 재할당이 필요한 경우에 한정해 사용하는 것이 좋다.
+// const 키워드를 사용하면 의도치 않은 재할당을 방지하기 때문에 좀 더 안전하다.
+// 1. ES6을 사용한다면 var 키워드는 사용하지 않는다.
+// 2. 재할당이 필요한 경우에 한정해 let 키워드를 사용한다. 이때 변수의 스코프는 최대한 좁게 만든다.
+// 3. 변경이 발생하지 않고 읽기 전용으로 사용하는(재할당이 필요 없는 상수) 원시 값과 객체에는 const 키워드를 사용한다. 
+//    const 키워드는 재할당을 금지하므로 var, let 키워드보다 안전하다.
+
+// 변수를 선언할 때는 일단 const 키워드를 사용하자! 반드시 재할당이 필요하다면 그때 const 키워드를 let 키워드로 변경해도 결코 늦지 않다!
