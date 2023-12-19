21b5e41e4a0230a13e0754b339c1b079de1d8ebf
kyw0300 <kyw0300@naver.com>
2023-12-20 00:11:32 +0900

    Feat:231220 18 firstClassObject.js 내용 추가 arguments, __proto__, prototype 프로퍼티

--- 18_firstClassObject/firstClassObject.js
+++ 18_firstClassObject/firstClassObject.js
@@ -57,3 +57,81 @@ console.log(Object.getOwnPropertyDescriptor(square, '__proto__')); // undefined
 // square 함수는 Object.prototype 객체로부터 __proto__ 접근자 프로퍼티를 상속받는다.
 console.log(Object.getOwnPropertyDescriptor(Object.prototype, '__proto__'));
 // {get: ƒ, set: ƒ, enumerable: false, configurable: true}
+
+
+// arguments 프로퍼티
+// arguments 프로퍼티의 값은 arguments 객체다. arguments 객체는 함수 호출 시 전달된 인수(argument)들의 정보를 담고 있는
+// 순회 가능한(iterable) 유사 배열 객체이며, 함수 내부에서 지역 변수처럼 사용된다.
+
+// 자바스크립트는 함수의 매개변수와 인수의 개수가 일치하는지 확인하지 않기 때문에 함수 호출 시
+// 매개변수 개수만큼 인수를 전달하지 않아도 에러가 발생하지 않는다.
+function multiply(x, y) {
+    console.log(arguments);
+
+    // 이터레이터
+    // arguments 객체의 Symbol(Symbol.iterator) 프로퍼티는 arguments 객체를 순회 가능한 자료구조인 이터러블(iterable)로
+    // 만들기 위한 프로퍼티다. Symbol.iterator를 프로퍼티 키로 사용한 메서드를 구현하는 것에 의해 이터러블이 된다.
+    const iterator = arguments[Symbol.iterator]();
+
+    // 이터레이터의 next 메서드를 호출하여 이터러블 객체 arguments를 순회
+    console.log(iterator.next()); // {value: 1, done: false}
+    console.log(iterator.next()); // {value: 2, done: false}
+    console.log(iterator.next()); // {value: 3, done: false}
+    console.log(iterator.next()); // {value: undefined, done: true}
+
+    return x * y;
+}
+
+console.log(multiply());        // NaN
+console.log(multiply(1));       // NaN
+console.log(multiply(1, 2));    // 2
+console.log(multiply(1, 2, 3)); // 2
+
+
+// 함수가 호출되면 인수 개수를 확인하고 이에 따라 함수의 동작을 달리 정의할 필요가 있을 때,
+// 매개변수 개수를 확정할 수 없는 가변 인자 함수를 구현할 때 arguments 객체가 유용하게 쓰임
+function sum() {
+    let res = 0;
+
+    // arguments 객체는 length 프로퍼티가 있는 유사 배열 객체이므로 for 문으로 순회할 수 있다.
+    for (let i = 0; i < arguments.length; i++) {
+        res += arguments[i];
+    }
+
+    return res;
+}
+
+console.log(sum());        // 0
+console.log(sum(1, 2));    // 3
+console.log(sum(1, 2, 3)); // 6
+
+
+// 함수 이름과 함수 객체를 가리키는 식별자는 의미가 다르다는 것을 잊지 말것!
+// 함수를 호출할 때는 함수 이름이 아닌 함수 객체를 가리키는 식별자로 호출한다.
+
+
+// __proto__ 프로퍼티
+// __proto__ 프로퍼티는 [[Prototype]] 내부 슬롯이 가리키는 프로토타입 객체에 접근하지 위해 사용하는 접근자 프로퍼티다.
+// 내부 슬롯에는 직접 접근할 수 없고 간접적인 접근 방법을 제공하는 경우에 한하여 접근할 수 있다.
+// [[Prototype]] 내부 슬롯에도 직접 접근할 수 없으며 __proto__ 접근자 프로퍼티를 통해 간접적으로 프로토타입 객체에 접근할 수 있다.
+
+const obj = { a: 1 };
+
+// 객체 리터럴 방식으로 생성한 객체의 프로토타입 객체는 Object.prototype이다.
+console.log(obj.__proto__ === Object.prototype); // true
+
+// 객체 리터럴 방식으로 생성한 객체는 프로토타입 객체인 Object.prototype의 프로퍼티를 상속받는다.
+// hasOwnProperty 메서드는 Object.prototype의 메서드다.
+console.log(obj.hasOwnProperty('a'));         // true
+console.log(obj.hasOwnProperty('__proto__')); // false
+
+// hasOwnProperty 메서드는 이름에서 알 수 있듯이 인수로 전달받은 프로퍼티 키가 객체 고유의 프로퍼티 키인 경우에만 true를
+// 반환하고 상속받은 프로토타입의 프로퍼티 키인 경우 false를 반환한다.
+
+
+// prototype 프로퍼티 : 생성자 함수로 호출할 수 있는 객체, 즉 constructor만이 소유하는 객체.
+// 함수 객체는 prototype 프로퍼티를 소유한다.
+(function () {}).hasOwnProperty('prototype'); // -> true
+
+// 일반 객체는 prototype 프로퍼티를 소유하지 않는다.
+({}).hasOwnProperty('prototype'); // -> false
