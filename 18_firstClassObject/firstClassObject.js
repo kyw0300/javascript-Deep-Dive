bad9ac71109061cf9f86667f9eed100c77514814
kyw0300 <kyw0300@naver.com>
2023-12-19 23:32:52 +0900

    Feat:231219 18 firstClassObject.js 추가 일급 객체, 함수 객체의 프로퍼티

---
+++ 18_firstClassObject/firstClassObject.js
@@ -0,0 +1,59 @@
+// 1. 함수는 무명의 리터럴로 생성할 수 있다.
+// 2. 함수는 변수에 저장할 수 있다.
+// 런타임(할당 단계)에 함수 리터럴이 평가되어 함수 객체가 생성되고 변수에 할당된다.
+const increase = function (num) {
+    return ++num;
+};
+
+const decrease = function (num) {
+    return --num;
+};
+
+// 2. 함수는 객체에 저장할 수 있다.
+const auxs = { increase, decrease };
+
+// 3. 함수의 매개변수에게 전달할 수 있다.
+// 4. 함수의 반환값으로 사용할 수 있다.
+function makeCounter(aux) {
+    let num = 0;
+
+    return function () {
+        num = aux(num);
+        return num;
+    };
+}
+
+// 3. 함수는 매개변수에게 함수를 전달할 수 있다.
+const increaser = makeCounter(auxs.increase);
+console.log(increaser()); // 1
+console.log(increaser()); // 2
+
+// 3. 함수는 매개변수에게 함수를 전달할 수 있다.
+const decreaser = makeCounter(auxs.decrease);
+console.log(decreaser()); // -1
+console.log(decreaser()); // -2
+
+
+function square(number) {
+    return number * number;
+}
+
+console.dir(square);
+console.log(Object.getOwnPropertyDescriptors(square));
+/*
+{
+  length: {value: 1, writable: false, enumerable: false, configurable: true},
+  name: {value: "square", writable: false, enumerable: false, configurable: true},
+  arguments: {value: null, writable: false, enumerable: false, configurable: false},
+  caller: {value: null, writable: false, enumerable: false, configurable: false},
+  prototype: {value: {...}, writable: true, enumerable: false, configurable: false}
+}
+*/
+
+// __proto__는 square 함수의 프로퍼티가 아니다.
+console.log(Object.getOwnPropertyDescriptor(square, '__proto__')); // undefined
+
+// __proto__는 Object.prototype 객체의 접근자 프로퍼티다.
+// square 함수는 Object.prototype 객체로부터 __proto__ 접근자 프로퍼티를 상속받는다.
+console.log(Object.getOwnPropertyDescriptor(Object.prototype, '__proto__'));
+// {get: ƒ, set: ƒ, enumerable: false, configurable: true}
